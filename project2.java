e43dc4acd71f58ad5ce4381804caa4a517998459
kiennguyenGH <ktnguyen4647@gmail.com>
2022-10-23 13:28:46 -0700

    Prepare jacobi

--- project2.java
+++ project2.java
@@ -1,5 +1,6 @@
 
 import java.util.Scanner;
+import java.lang.Math;
 import java.io.File;
 import java.io.FileNotFoundException;
 
@@ -42,15 +43,82 @@ public class project2
                 rows[i][k] = Double.parseDouble(splitTest[k]);
             }
         }
-
-        scan.close();
+        // scan.close();
         return rows;
+    }
+
+    public static void printArray(double[] array)
+    {
+        for (int i = 0; i < array.length; i++)
+        {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
     }
 
     public static void jacobi(double[][] matrix)
     {
+        Scanner scanner = new Scanner(System.in);
         double[][] theMatrix = matrix;
         double[] solution = new double[theMatrix.length];
+        boolean isDiagonal = true;
+        for (int i = 0; i < theMatrix.length; i++)
+        {
+            double sum = 0;
+            for(int k = 0; k < theMatrix[i].length-1; k++)
+            {
+                if (k == i)
+                {
+                    continue;
+                }
+                else
+                {
+                    sum += Math.abs(theMatrix[i][k]);
+                }
+            }
+            if (sum > theMatrix[i][i])
+            {
+                isDiagonal = false;
+                System.out.println("Cannot perform Jacobi Iteration: Matrix is not diagonally dominant");
+                break;
+            }
+        }
+        if (isDiagonal)
+        {
+            
+            System.out.println("Enter the starting solution: ");
+            String input = scanner.nextLine();
+            // while (scan.hasNextLine())
+            // {
+            //     input = scan.nextLine();
+            // }
+            String[] split = input.split("\\s+");
+            System.out.println("Enter the desired stopping error: ");
+            double stoppingError = scanner.nextDouble();
+            for (int i = 0; i < solution.length; i++)
+            {
+                solution[i] = Double.parseDouble(split[i]);
+            }
+            for (int i = 0; i < 50; i++)
+            {
+                
+                for (int k = 0; k < solution.length; k++)
+                {
+                    double sum = 0;
+                    for (int j = 0; j < theMatrix[k].length-1; j++)
+                    {
+                        if (k != j)
+                        {
+                            sum += (theMatrix[k][j] * solution[j]);
+                        }
+                    }
+                    solution[k] = (theMatrix[k][theMatrix[k].length-1] - sum) / theMatrix[k][k];
+                }
+                printArray(solution);
+            }
+            
+        }
+        // scanner.close();
     }
 
     public static void main(String[] args)
@@ -61,7 +129,7 @@ public class project2
        {
             System.out.println("Enter:\n1. To input the equations\n2. To read from a file");
             theInput = input.nextInt();
-            input.nextLine();
+            // input.nextLine();
             if (theInput != 1 && theInput != 2)
             {
                 System.out.println("Invalid input");
@@ -73,7 +141,7 @@ public class project2
        if (theInput == 1)
        {
             double[][] test = getMatrix();
-            // computeGaussian(test);
+            jacobi(test);
         
        }
        //Read file
